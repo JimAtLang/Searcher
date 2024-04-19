fcabcd70104cab4e5aaedea851a62646e18230d0
jimatlang <jim.stewart@thelangschool.org>
2024-04-19 09:58:43 -0400

    starter project

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args){
+        new Main();
+    }
+    public Main(){
+        Random r = new Random();
+        int searchSpaceSize = 1000;
+        int maxValue = searchSpaceSize;
+        int[] searchSpace = new int[searchSpaceSize];
+        for(int i=0;i<searchSpace.length;i++){
+            searchSpace[i] = r.nextInt(maxValue);
+            // System.out.println("Item " + i + " is " + searchSpace[i]);
+        }
+        int searchTerm = 0;
+        if(r.nextBoolean()){
+            searchTerm = searchSpace[r.nextInt(searchSpace.length)];
+        } else {
+            searchTerm = r.nextInt(maxValue);
+        }
+        Searcher s = new Searcher();
+        int index = s.linearSearch(searchSpace, searchTerm);
+        if(index<0){
+            System.out.println("The term " + searchTerm + " is not in the search space");
+        } else {
+            System.out.println("The term " + searchTerm + " is at position " + index + " in  the search space");
+        }
+    }
+}
---
+++ Searcher.java
@@ -0,0 +1,8 @@
+public class Searcher {
+    public int linearSearch(int[] searchSpace, int searchTerm){
+        // If the term is in the search space, find the index 
+        // of the term. If it is not in the search space, 
+        // return -1
+        return 0;
+    }
+}
