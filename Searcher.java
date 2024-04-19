02f831a963477f5f1188f6c4bb7ec4580095f43c
Carny2575 <milespilchik@gmail.com>
2024-04-19 10:45:34 -0400

    Finished Work!

--- Searcher.java
+++ Searcher.java
@@ -3,6 +3,26 @@ public class Searcher {
         // If the term is in the search space, find the index 
         // of the term. If it is not in the search space, 
         // return -1
-        return 0;
+
+        int i = 0;
+    
+
+        if (searchSpace == null) { 
+            return -2; 
+        } 
+       
+        while (i < searchSpace.length) { 
+  
+          
+            if (searchSpace[i] == searchTerm) { 
+                return i; 
+            } 
+            else { 
+
+            i++;
+                
+            } 
+        } 
+        return -1; 
     }
 }
